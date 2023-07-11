45d903af9b3518aeedf23db24ff59d253432960c
Mithun.U <116871732+Mithun-750@users.noreply.github.com>
2023-07-11 20:57:11 +0530

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import './Profile.css';
 import Notecontext from '../../context/notes/Notecontext';
 
